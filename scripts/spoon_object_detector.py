e1d0ffaf0844cad8ba6f4ed619c4be05323ec5fd
Travers Rhodes <traversr@andrew.cmu.edu>
2018-08-03 18:43:17 -0400

    move histogram commands to a utilities file

--- scripts/spoon_object_detector.py
+++ scripts/spoon_object_detector.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 
 import spoon_camera_interface as sci
+import histogram_utilities as hu
 
 from spoon_perception.srv import ObjectSpoon, ObjectSpoonResponse
 
@@ -35,7 +36,7 @@ class SpoonObjectDetector:
     distances = []
     for i in range(self.number_frames):
         image = self.spoon_camera.getImage() 
-        histogram = getHSVHistogram(image, self.spoon_camera.spoon_mask)
+        histogram = hu.getHSVHistogram(image, self.spoon_camera.spoon_mask)
         d = cv2.compareHist(self.spoon_histogram['H'], histogram['H'],
                             cv2.cv.CV_COMP_CORREL)
         distances.append(d)
@@ -59,7 +60,7 @@ class SpoonObjectDetector:
                             '--- Press ENTER to accept the image'
                             '--- Press ANY key to reject')
     
-    histogram = getHSVHistogram(image, self.spoon_camera.spoon_mask)
+    histogram = hu.getHSVHistogram(image, self.spoon_camera.spoon_mask)
 
     return histogram
 
@@ -77,50 +78,6 @@ def displayWait(image,window_name='default'):
 
   cv2.destroyAllWindows()
   return image_accepted
-
-def getHSVHistogram(image, mask=None):
-  """
-  function that gets the HSV histogram of a bgr8 image, optinaly can include
-  a mask for the image
-  returns in the form of a dictionary with H: S: V:
-  """
-
-  hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-  hist_H = cv2.calcHist( [hsv_image], [0], mask, [180], [0, 179])
-  hist_S = cv2.calcHist( [hsv_image], [1], mask, [256], [0, 255])
-  hist_V = cv2.calcHist( [hsv_image], [2], mask, [256], [0, 255])
-
-  histogram = {'H':hist_H, 'S':hist_S, 'V':hist_V}
-
-  return histogram
-
-def histogramHSVPlot(histogram):
-  """
-  plot the HSV histogram, histogram must be a dictionary with H: S: V:
-  """
-
-  hist_H = histogram['H']
-  hist_S = histogram['S']
-  hist_V = histogram['V']
-
-  plt.subplot(3, 1, 1)
-  plt.plot(hist_H)
-  plt.xlim([0, 179])
-  plt.title('Histograma H')
-
-  plt.subplot(3, 1, 2)
-  plt.plot(hist_S)
-  plt.xlim([0, 255])
-  plt.title('Histograma S')
-
-  plt.subplot(3, 1, 3)
-  plt.plot(hist_V)
-  plt.xlim([0, 255])
-  plt.title('Histograma V')
-
-  plt.show()
-  return
 
 if __name__ == '__main__':
   rospy.init_node('spoon_object_detector',anonymous = True)
